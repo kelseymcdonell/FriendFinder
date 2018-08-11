21261e97eef11cae801d2951b9577db4e25ba927
kelsey <kelseymcdonell@gmail.com>
2018-08-10 20:12:59 -0400

    App

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: "Andy",
+		photo: "https://vignette.wikia.nocookie.net/theoffice/images/b/b5/Andy_Bernard.jpg/revision/latest?cb=20170701084205",
+		scores: [5, 1, 2, 3, 1, 2, 4, 1, 4, 3]
+	},
+	{
+		name: "Michael",
+		photo: "https://en.wikipedia.org/wiki/Michael_Scott_(The_Office)#/media/File:MichaelScott.png",
+		scores: [2, 2, 5, 2, 2, 3, 3, 4, 3, 5]
+	},
+	
+	{
+		name: "Dwight",
+		photo: "https://en.wikipedia.org/wiki/Dwight_Schrute#/media/File:Dwight_Schrute.jpg",
+		scores: [5, 2, 3, 4, 1, 1, 1, 2, 5, 3]
+	}
+];
+
+module.exports = friends;
